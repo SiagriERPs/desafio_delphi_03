06f113a19ac0b7ec9b1c4de498aa658f76d7ad33
Cristianars <67428454+Cristianars@users.noreply.github.com>
2022-05-28 02:07:43 -0300

    docs e inicio do projeto
    scripts criação do banco de dados/tabelas

---
+++ src/Projeto/Controle_Negociação.dpr
@@ -0,0 +1,14 @@
+program Controle_Negociação;
+
+uses
+  Vcl.Forms,
+  uPesquisa in '..\uPesquisa.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
