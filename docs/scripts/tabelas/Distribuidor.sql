7e2fcdad9a1d090ed1d7dbd1b7a12227dbeb7e55
Vinicius Cintra Domingos <vdomingos.dev@gmail.com>
2024-11-13 16:00:50 -0300

    Revert "Desafio Aliare - Junismar"

--- docs/scripts/tabelas/Distribuidor.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tab_distribuidor (
-    distribuidor_id INTEGER NOT NULL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cnpj VARCHAR(20) UNIQUE NOT NULL
-);
--- docs/scripts/triggers/trg_tab_produto_id.sql
+++
@@ -1,7 +0,0 @@
-CREATE TRIGGER trg_tab_produto_id FOR tab_produto
-BEFORE INSERT
-AS
-BEGIN
-  IF (NEW.produto_id IS NULL) THEN
-    NEW.produto_id = GEN_ID(gen_tab_produto_id, 1);
-END;
